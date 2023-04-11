fd80a3f9990315a92308a17b7aad0f266f2e0c00
NGKCHI <129876379+NGKCHI@users.noreply.github.com>
2023-04-11 22:31:50 +0700

    Update DSHD.h

--- DSHD.h
+++ DSHD.h
@@ -16,3 +16,40 @@ public:
 	void xem_hd(int ngay, int thang, int nam);
 };
 
+DSHD::DSHD(int n)
+{
+	HD = new HoaDon[n + 1];
+	this->size = n;
+}
+
+DSHD::~DSHD()
+{
+	delete[] HD;
+}
+
+void DSHD::nhap()
+{
+	for (int i = 1; i <= size; i++)
+	{
+		HD[i].nhap();
+	}
+}
+void DSHD::xuat()
+{
+	for (int i = 1; i <= size; i++)
+	{
+		HD[i].xuat();
+	}
+}
+
+// Xem toàn bộ hóa đơn trong ngày cụ thể
+void DSHD::xem_hd(int ngay, int thang, int nam)
+{
+	for (int i = 1; i <= size; i++)
+	{
+		if(HD[i].get_ngay_xuat() == ngay && HD[i].get_thang_xuat() == thang && HD[i].get_nam_xuat() == nam)
+		{
+			HD[i].xuat();
+		}
+	}
+}
