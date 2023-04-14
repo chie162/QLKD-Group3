09321c4ddb94ad96faab49c39d460f7e23033a4d
PhamNgocAnhh <129879316+PhamNgocAnhh@users.noreply.github.com>
2023-04-14 23:29:23 +0700

    Update TaiChinh.h

--- TaiChinh.h
+++ TaiChinh.h
@@ -10,7 +10,15 @@ class TaiChinh
 	public:
 	TaiChinh();
 	~TaiChinh();
-	double tinh_loi_nhuan(); 
+	 void nhap();
+	 void xuat();
+	 double get_doanh_thu();
+	 double get_chi_phi();
+	 double get_loi_nhuan();
+	 void set_doanh_thu();
+	 void set_chi_phi();
+	 void set_loi_nhuan();
+	 double tinh_loi_nhuan(); 
  };
  TaiChinh::TaiChinh()
  {
@@ -21,8 +29,31 @@ class TaiChinh
  TaiChinh::~TaiChinh()
  {
   }
+void TaiChinh::nhap()
+{
+	cout<<"Nhap doanh thu: "; cin>>doanh_thu;
+ 	cout<<"Nhap chi phi: "; cin>>chi_phi;
+	loi_nhuan=doanh_thu-chi_phi;
+}
+void TaiChinh::xuat()
+{cout<<"Doanh thu la: "<<doanh_thu<<endl;
+ cout<<"Chi phi la: "<<chi_phi<<endl;
+ cout<<"Loi nhuan la: "<<loi_nhuan<<endl;}
+double TaiChinh::get_doanh_thu()
+{return this->doanh_thu;}
+double TaiChinh::get_chi_phi()
+{return this->chi_phi;}
+double TaiChinh::get_loi_nhuan()
+{return this->loi_nhuan;}
+void TaiChinh::set_doanh_thu(double doanh_thu)
+{this->doanh_thu=doanh_thu;}
+void TaiChinh::set_chi_phi(double chi_phi)
+{this->chi_phi=chi_phi;}
+void TaiChinh::set_loi_nhuan(double loi_nhuan)
+{this->loi_nhuan=loi_nhuan;}
+	
  double TaiChinh::tinh_loi_nhuan() 
  {
- 	
+return this->loi_nhuan;
  }
  
