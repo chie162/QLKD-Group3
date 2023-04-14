3f62b4a2f900822204b50ed0853610ea5b8fbeed
PhamNgocAnhh <129879316+PhamNgocAnhh@users.noreply.github.com>
2023-04-14 21:18:38 +0700

    Add files via upload

---
+++ TaiChinh.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+class TaiChinh
+{
+	private:
+	double doanh_thu;
+	double chi_phi;
+	double loi_nhuan;
+	public:
+	TaiChinh();
+	~TaiChinh();
+	double tinh_loi_nhuan(); 
+ };
+ TaiChinh::TaiChinh()
+ {
+ 	doanh_thu =0;
+	chi_phi=0;
+	loi_nhuan=0; 
+  } 
+ TaiChinh::~TaiChinh()
+ {
+  }
+ double TaiChinh::tinh_loi_nhuan() 
+ {
+ 	
+ }
+ 
