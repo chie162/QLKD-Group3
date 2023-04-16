0b30980d9fdb0cf71d165b4fe4f7b8401ebc9f5a
NgocAnhTranThi <129878354+NgocAnhTranThi@users.noreply.github.com>
2023-04-16 21:28:19 +0700

    Update DSNV.h

--- DSNV.h
+++ DSNV.h
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-class DSNV 
+class DSNV : public DanhSach 
 {
     private:
         NhanVien * DanhSach = nullptr;
@@ -18,54 +18,49 @@ class DSNV
         void nhap();
         void xuat();
         void xem_dsnv(int n);
-        void nhap1nv();
+        double get_luong();
         void xem_luongnv(int i);
 }; 
 
 DSNV::DSNV(int n)
 {
-    NV = new NhanVien[n + 1];
+    danh_sach = new NhanVien[n + 1];
     this->size = n;
 }
 
 DSNV::~DSNV()
 {
-    delete[] NV; 
+    delete[] danh_sach; 
 }
 
 void DSNV::nhap(int i)
 {
     for (int i=0; i < size; i++)
     {
-        NV[i].nhap();
+        danh_sach[i].nhap();
     }
 }
 void DSNV::xuat(int i)
 {
     for (int i=0; i < size; i++)
     {
-        NV[i].xuat();
+        danh_sach[i].xuat();
     }
 }
 void DSNV::xem_dsnv(int n)
 {
     for (int i=0; i < size; i++)
     {
-        NV[i].nhap1nv();
-        NV[i].xuat1nv();
+        danh_sach[i].xem_dsnv();
     }
 }
 void DSNV::xem_luongnv(int i)
-{
-    int m=0; 
+{ 
     for(int i=0; i < size; i++)
     {
-        if(NV[i].luong>=5000000)
-        {
-            m++;
-        }
+        if(danh_sach[i].get_luong() >= 5000000)
+            danh_sach[i].xuat();
     }
-    cout<<"co "<<m<<"danh sach nhan vien co muc luong >=5000000 "<<endl;
 }   
 
     
