f4639c82ec00369453d71af6cac33830440ff64a
NgocAnhTranThi <129878354+NgocAnhTranThi@users.noreply.github.com>
2023-04-16 21:01:02 +0700

    Update NhanVien.h

--- NhanVien.h
+++ NhanVien.h
@@ -18,9 +18,9 @@ class NhanVien : public Nguoi
 
         void nhap(int n);
         void xuat(int n);
-        void chinh_sua(char choice);
+        void chinh_sua(int i);
        
-        double get_luong() {return this->luong = luong; }
+        double get_luong() {return this->luong;}
 
 
         void set_ngay_vao_lam(Ngay ngay_vao_lam) {this->ngay_vao_lam = ngay_vao_lam; }
@@ -42,26 +42,26 @@ NhanVien::~NhanVien()
 
 void NhanVien::nhap(int n)
 {
-    Nguoi::nhap(); 
-    cout<<"Nhap ngay vao lam: "; this->ngay_vao_lam.nhap(); cin.ignore();
-    cout<<"Nhap luong:"; getline(cin, this->luong);
+    Nguoi::nhap(int n); 
+    cout<<"Nhap ngay vao lam: "; this->ngay_vao_lam.nhap(); 
+    cout<<"Nhap luong:"; cin>>luong;
 }
 
 
 void NhanVien::xuat(int n)
 {
-    Nguoi::xuat();
-    cout<<"Ngay vao lam:"; ngay_vao_lam.xuat();
-    cout<<"Luong:"<< luong << endl;
+    Nguoi::xuat(int n);
+    cout<<"Ngay vao lam la:"; ngay_vao_lam.xuat();
+    cout<<"Luong la:"<< luong << endl;
 }
 
-void NhanVien::chinh_sua(char choice)
+void NhanVien::chinh_sua(int i)
 { 
     do{
         cout<<"Nhap ten nhan vien:"; cin>>ten;
         cout<<"Nhap ma nhan vien:"; cin>>ma_nv;
-        cout<<"Ban co muon chinh sua thong tin nhan vien nay:"\n[Y]es\t[N]o\n; cin>>choice; 
+        cout<<"Ban co muon chinh sua thong tin nhan vien nay:"\n[Y]es\t[N]o\n; cin>>i; 
     }
-    while ((choice == 'Y') || (choice == 'N'));
+    while ((i == 'Y') || (i == 'N'));
     return 0; 
 }
