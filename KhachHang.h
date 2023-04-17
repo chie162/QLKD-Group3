4c52b269aaac57243529f27ee1cb8dbc0e28f8e6
PhamNgocAnhh <129879316+PhamNgocAnhh@users.noreply.github.com>
2023-04-17 23:38:25 +0700

    Update KhachHang.h

--- KhachHang.h
+++ KhachHang.h
@@ -6,27 +6,30 @@ using namespace std;
 class KhachHang:public Nguoi 
 {
 	private:
-	string ngay_dang_ki;
-	double so_tien_da_chi; 
+		string ngay_dang_ki;
+		double so_tien_da_chi; 
 	public:
-	KhachHang();
-	~KhachHang();
-	void nhap();
-	void xuat();
-	double get_so_tien_da_chi();
-	void set_ngay_dang_ki();
-	void set_so_tien_da_chi();	 
+		KhachHang(string ngay_dang_ki="None", double so_tien_da_chi=0);
+	
+		~KhachHang();
+	
+		void nhap(int i);
+		void xuat(int i);
+	
+		double get_so_tien_da_chi();
+		void set_ngay_dang_ki(Ngay ngay_dang_ki);
+		void set_so_tien_da_chi(string so_tien_da_chi);	 
  };
- KhachHang::KhachHang()
+ KhachHang::KhachHang(string ngay_dang_ki="None", double so_tien_da_chi=0)
  	{ngay_dang_ki=" "; so_tien_da_chi=0;}
  KhachHang::~KhachHang(){}
- void KhachHang::nhap()
+ void KhachHang::nhap(int i)
 	 {
  		Nguoi::nhap();
 		cout<<"Nhap ngay dang ki: "; fflush(stdin); getline(cin, ngay_dang_ki);
 		cout<<"Nhap so tien da chi: "; cin>>so_tien_da_chi;
  	 } 
-void KhachHang::xuat()
+void KhachHang::xuat(int i)
 	{
 		Nguoi::xuat();
 		cout<<"Ngay dang ki la: "<<ngay_dang_ki<<endl;
@@ -36,7 +39,7 @@ double KhachHang::get_so_tien_da_chi()
 	{
 		return this->so_tien_da_chi; 
 	 } 
-void KhachHang::set_ngay_dang_ki(string ngay_dang_ki)
+void KhachHang::set_ngay_dang_ki(Ngay ngay_dang_ki)
 	{
 		this->ngay_dang_ki=ngay_dang_ki; 
 	 } 
